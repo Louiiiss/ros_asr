61f77414af047a3a0d9fab5cf02e19fa2d64017c
Louis <lthorpe-monaghan1@sheffield.ac.uk>
2017-07-21 14:31:46 +0100

    Made some refinements

--- run-ros-asr-baxter.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd src
-#gnome-terminal -e rosrun ros_asr kaldi_relay
-#gnome-terminal -e 'sh -c (../bin/kaldi_relay)'
-roslaunch ros_asr end_effector_control.launch
-
--- src/install_packages.sh
+++ src/install_packages.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 apt-get install subversion make g++ libasound2-dev gfortran sphinxbase-utils python-dev python-numpy gawk swig libatlas3-base
+
+sudo apt-get install ros-kinetic-trac-ik
+
+su
